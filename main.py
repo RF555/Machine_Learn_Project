0268238e131963252735d6caec1ef07b706c23df
Roey Feingold <roey.feingold@gmail.com>
2023-01-15 19:22:55 +0200

    update project

--- main.py
+++ main.py
@@ -7,8 +7,15 @@ Beatles_playlist = "4L21s7bRV7QcUq1YaPobwB"
 Disney_mix_playlist = "6UycuQgQHbt0bpS4sat8xc"
 Queen_Best_Of_playlist = "595cY5S1FpMpiEIWsk6dG7"
 
+FOLK_ACUSTIC_MIX = "37i9dQZF1EQp62d3Dl7ECY"
+POP_MIX = "37i9dQZF1EQncLwOalG3K7"
+METAL_MIX = "37i9dQZF1EQpgT26jgbgRI"
+ROCK_MIX = "37i9dQZF1EQpj7X7UK8OOF"
+HIP_HOP_MIX = "37i9dQZF1EQnqst5TRi17F"
+COUNTRY_MIX = "37i9dQZF1EQmPV0vrce2QZ"
+
 if __name__ == '__main__':
-    getPlaylistData(Passenger_playlist)
+    getPlaylistData(ROCK_MIX)
     # CLIENT = pd.read_csv("Spotify Clients.csv")
     # CLIENT_ID = CLIENT.iloc[0][0]  # CLIENT ID
     # CLIENT_SECRET = CLIENT.iloc[0][1] # CLIENT SECRET
